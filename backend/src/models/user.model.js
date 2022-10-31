b3397b9190aeb4ccef6e45e5608f6107dfd29bdf
chamathjayasekara99 <it20654962@my.sliit.lk>
2022-11-01 02:40:57 +0530

    public tranport manager function MVC added but some methods needs to be modified

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -2,49 +2,53 @@ import mongoose from 'mongoose';
 import mongoosePaginate from 'mongoose-paginate-v2';
 
 const UserSchema = new mongoose.Schema(
-    {
-        role: {
-            type: String,
-            enum: ["LOCALPASSENGER", "FORIEGNPASSENGER", "INSPECTOR"],
-            default: "LOCALPASSENGER",
-        },
-        LocalPassenger: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "LocalPassenger",
-        },
-        ForeignPassenger: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "ForeignPassenger",
-        },
-        Inspector: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "Inspector",
-        },
-        name: {
-            type: String,
-            required: true,
-        },
-        email: {
-            type: String,
-            required: true,
-            unique: true,
-            lowercase: true,
-        },
-        password: {
-            type: String,
-            required: true,
-            minlength: 8,
-        },
-        contactNumber: {
-            type: Number,
-            required: true,
-            minlength: 8,
-        }
+  {
+    role: {
+      type: String,
+      enum: ["LOCALPASSENGER", "FORIEGNPASSENGER", "INSPECTOR","PTMANAGER"],
+      default: "LOCALPASSENGER",
     },
-    {
-        versionKey: false,
-        timestamps: {createdAt: 'created_at', updatedAt: 'updated_at'},
+    LocalPassenger: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "LocalPassenger",
     },
+    ForeignPassenger: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "ForeignPassenger",
+    },
+    Inspector: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Inspector",
+    },
+    PTManager: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "PTManager",
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 8,
+    },
+    contactNumber: {
+      type: Number,
+      required: true,
+      minlength: 8,
+    },
+  },
+  {
+    versionKey: false,
+    timestamps: { createdAt: "created_at", updatedAt: "updated_at" },
+  }
 );
 
 UserSchema.plugin(mongoosePaginate);
--- backend/src/routes/index.routes.js
+++ backend/src/routes/index.routes.js
@@ -1,10 +1,12 @@
-import express from 'express';
-import authRouter from './auth.routes';
-import userRouter from './user.routes';
-
+import express from "express";
+import authRouter from "./auth.routes";
+import userRouter from "./user.routes";
+import publicTransportProviderRouter from "./publicTransportProvider.routes";
+import transportRouteRouter from "./transportRoute.routes";
 const router = express.Router();
 
-router.use('/auth', authRouter);
-router.use('/user', userRouter);
-
+router.use("/auth", authRouter);
+router.use("/user", userRouter);
+router.use("/publicTransportProvider", publicTransportProviderRouter);
+router.use("/traportRoute", transportRouteRouter);
 export default router;
