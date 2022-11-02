c93e44448777c84b28a571540b0a360d250816f7
ThulinaWickramasinghe <thulinawickramasinghe@gmail.com>
2022-11-02 20:30:04 +0530

    Add dummy finance data

---
+++ public-transport-manager-react-app/src/data/dummyFinanceData.js
@@ -0,0 +1,57 @@
+export const financeData = [
+    {
+        date : "2022/10/5",
+        time : "06:00 - 07:15",
+        from : "Kegalle",
+        to : "Kurunegala",
+        income : 10000,
+        expenses : 5000,
+        profit : 5000
+    },
+    {
+        date : "2022/10/5",
+        time : "07:30 - 08:45",
+        from : "Kurunegala",
+        to : "Kegalle",
+        income : 7500,
+        expenses : 5000,
+        profit : 5000
+    },
+    {
+        date : "2022/10/5",
+        time : "09:00 - 12:00",
+        from : "Kegalle",
+        to : "Colombo",
+        income : 10000,
+        expenses : 10000,
+        profit : 0
+    },
+    {
+        date : "2022/10/6",
+        time : "06:00 - 07:15",
+        from : "Kegalle",
+        to : "Kurunegala",
+        income : 15000,
+        expenses : 5000,
+        profit : 5000
+    },
+    {
+        date : "2022/10/7",
+        time : "07:30 - 08:45",
+        from : "Kurunegala",
+        to : "Kegalle",
+        income : 7500,
+        expenses : 5000,
+        profit : 5000
+    },
+    {
+        date : "2022/10/8",
+        time : "09:00 - 12:00",
+        from : "Kegalle",
+        to : "Colombo",
+        income : 19000,
+        expenses : 10000,
+        profit : 10000
+    },
+
+]
