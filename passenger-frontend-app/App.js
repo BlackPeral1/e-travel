2f5aa048ded752ed3d554bd7eee2ffcc46b4b55a
Ravindu Nirmal <ravindu.nirmal099@gmail.com>
2022-11-03 19:26:16 +0530

    App.js Updated

--- passenger-frontend-app/App.js
+++ passenger-frontend-app/App.js
@@ -1,15 +1,8 @@
 import React from 'react';
-// import AuthStack from "./src/navigation/AuthStack";
-
-
 import AuthStack from "./src/navigation/AuthStack";
-import PaymentStack from "./src/navigation/PaymenStack";
 
 export default function App() {
     return (
-
         <AuthStack/>
-        // <PaymentStack/>
-
     )
 }
